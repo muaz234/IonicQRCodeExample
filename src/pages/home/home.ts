9bce1c9aeabb8401cc9471bcb4d1b0fdccf77883
Muaz <muaz@example.com>
2018-11-13 23:17:45 +0800

    barcode scanner function- subject to test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, NavParams } from 'ionic-angular';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+export class HomePage {
+scanData : {};
+options: BarcodeScannerOptions;
+encodeData : string ;
+encodedData : {};
+  constructor(public navCtrl: NavController, public navParams: NavParams, private barcodeScanner: BarcodeScanner ) {
 
   }
+
+  scan(){
+    this.options = {
+      prompt: "Scan your barcode"
+    }
+    this.barcodeScanner.scan(this.options).then((barcodeData) => {
+      console.log(barcodeData);
+      this.scanData = barcodeData;
+    }, (err) => {
+      console.log("Error occured :" + err);
+    });
+
+  }
+
+  encodeText(){
+    this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE, this.encodeData).then((encodedData) => {
+      console.log(encodedData);
+    }, (err) => {
+      console.log("Error occured :" + err);
+    });
+  }
+  
+
+
 
 }
